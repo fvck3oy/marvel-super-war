fdbcb179b55532cfa9856568cc8613bd9e2eb9d2
pumin <pumin@weserve.co.th>
2020-03-24 16:16:52 +0700

    Updates

---
+++ precache-manifest.dc52fb729b779d25fc61f29d70513112.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a9b5c8d6d922c5450e622caa240af9f",
+    "url": "/marvel-super-war/index.html"
+  },
+  {
+    "revision": "78a707e7e6de1a1e877e",
+    "url": "/marvel-super-war/static/css/2.e4aec663.chunk.css"
+  },
+  {
+    "revision": "3be44ecd1d9e42dda98e",
+    "url": "/marvel-super-war/static/css/main.291306f9.chunk.css"
+  },
+  {
+    "revision": "78a707e7e6de1a1e877e",
+    "url": "/marvel-super-war/static/js/2.1a145ce3.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/marvel-super-war/static/js/2.1a145ce3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3be44ecd1d9e42dda98e",
+    "url": "/marvel-super-war/static/js/main.887d770c.chunk.js"
+  },
+  {
+    "revision": "c9df7a0214b5873cd902",
+    "url": "/marvel-super-war/static/js/runtime-main.750a8a56.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/marvel-super-war/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/marvel-super-war/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/marvel-super-war/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/marvel-super-war/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/marvel-super-war/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
